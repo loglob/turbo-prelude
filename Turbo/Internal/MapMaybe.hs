e486a0e0b9dfbe9f2dfab9ef4be544b0e30d2a0f
loglob <23386237+loglob@users.noreply.github.com>
2024-05-17 02:39:37 +0200

    Use TH to generate lens-based map functions  + add new maps* variants

--- Turbo/Internal/MapMaybe.hs
+++ Turbo/Internal/MapMaybe.hs
@@ -1,8 +1,10 @@
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+
 -- | Lots of `mapMaybe*` variants
 module Turbo.Internal.MapMaybe where
 
 import Turbo.Internal.Classes
-import Turbo.Operators hiding ((?:>))
+import Turbo.Internal.TH
 import Turbo.RootPrelude
 
 (?:<) :: (Cons ys ys y y) => Maybe y -> ys -> ys
@@ -13,121 +15,57 @@ import Turbo.RootPrelude
 (?:>) ys Nothing = ys
 (?:>) ys (Just y) = ys `snoc` y
 
--- * `mapMaybe` variants
+-- * `mapsMaybe` pre-/appending variants
 
--- ** With index, with Maybe, inside monad
-
-mapMaybeIxLM :: (Enum i, Uncons xs x, AsEmpty ys, Cons ys ys y y, Monad m) => (i -> x -> m (Maybe y)) -> xs -> m ys
-{-# INLINE mapMaybeIxLM #-}
-mapMaybeIxLM f = m (toEnum 0)
+{- | Reduces from left to right, applies an optional mapping using indices, and prepends to a tail
+    Computations are evaluated with ascending indices
+-}
+mapsMaybeIxLM :: (Enum i, Uncons xs x, Cons ys ys y y, Monad m) => (i -> x -> m (Maybe y)) -> xs -> ys -> m ys
+{-# INLINE mapsMaybeIxLM #-}
+mapsMaybeIxLM f xs0 ys0 = m (toEnum 0) xs0
   where
     m i xs = case uncons xs of
-        Nothing -> return Empty
+        Nothing -> return ys0
         Just (x, rs) -> do
             y <- f i x
             ys <- m (succ i) rs
             return $ y ?:< ys
 
-mapMaybeIxLRM :: (Enum i, Uncons xs x, AsEmpty ys, Snoc ys ys y y, Monad m) => (i -> x -> m (Maybe y)) -> xs -> m ys
-{-# INLINE mapMaybeIxLRM #-}
-mapMaybeIxLRM f = m (toEnum 0) Empty
+{- | Reduces from left to right, applies an optional mapping using indices, and appends to a prefix
+    Computations are evaluated with ascending indices
+-}
+mapsMaybeIxLRM :: (Enum i, Uncons xs x, Snoc ys ys y y, Monad m) => (i -> x -> m (Maybe y)) -> xs -> ys -> m ys
+{-# INLINE mapsMaybeIxLRM #-}
+mapsMaybeIxLRM f = m (toEnum 0)
   where
-    m i ys xs = case uncons xs of
+    m i xs ys = case uncons xs of
         Nothing -> return ys
         Just (x, rs) -> do
             y <- f i x
-            m (succ i) (ys ?:> y) rs
+            m (succ i) rs (ys ?:> y)
 
-mapMaybeIxRM :: (Enum i, Unsnoc xs x, AsEmpty ys, Snoc ys ys y y, Monad m) => (i -> x -> m (Maybe y)) -> xs -> m ys
-{-# INLINE mapMaybeIxRM #-}
-mapMaybeIxRM f = fmap snd . m
+{- | Reduces from right to left, applies an optional mapping using indices, and appends to a prefix
+    Computations are evaluated with ascending indices
+-}
+mapsMaybeIxRM :: (Enum i, Unsnoc xs x, Snoc ys ys y y, Monad m) => (i -> x -> m (Maybe y)) -> xs -> ys -> m ys
+{-# INLINE mapsMaybeIxRM #-}
+mapsMaybeIxRM f xs0 ys0 = fmap snd $ m xs0
   where
     m xs = case unsnoc xs of
-        Nothing -> return (toEnum 0, Empty)
+        Nothing -> return (toEnum 0, ys0)
         Just (rs, x) -> do
             (i, ys) <- m rs
             y <- f i x
             return (succ i, ys ?:> y)
 
--- ** With index, with maybe, outside monad
-
-mapMaybeIxL :: (Enum i, Uncons xs x, AsEmpty ys, Cons ys ys y y) => (i -> x -> Maybe y) -> xs -> ys
-{-# INLINE mapMaybeIxL #-}
-mapMaybeIxL f = runIdentity . mapMaybeIxLM (Identity .: f)
-
-mapMaybeIxLR :: (Enum i, Uncons xs x, AsEmpty ys, Snoc ys ys y y) => (i -> x -> Maybe y) -> xs -> ys
-{-# INLINE mapMaybeIxLR #-}
-mapMaybeIxLR f = runIdentity . mapMaybeIxLRM (Identity .: f)
-
-mapMaybeIxR :: (Enum i, Unsnoc xs x, AsEmpty ys, Snoc ys ys y y) => (i -> x -> Maybe y) -> xs -> ys
-{-# INLINE mapMaybeIxR #-}
-mapMaybeIxR f = runIdentity . mapMaybeIxRM (Identity .: f)
-
--- ** With index, inside monad
-
-mapIxLM :: (Enum i, Uncons xs x, AsEmpty ys, Cons ys ys y y, Monad m) => (i -> x -> m y) -> xs -> m ys
-{-# INLINE mapIxLM #-}
-mapIxLM f = mapMaybeIxLM (fmap Just .: f)
-
-mapIxLRM :: (Enum i, Uncons xs x, AsEmpty ys, Snoc ys ys y y, Monad m) => (i -> x -> m y) -> xs -> m ys
-{-# INLINE mapIxLRM #-}
-mapIxLRM f = mapMaybeIxLRM (fmap Just .: f)
-
-mapIxRM :: (Enum i, Unsnoc xs x, AsEmpty ys, Snoc ys ys y y, Monad m) => (i -> x -> m y) -> xs -> m ys
-{-# INLINE mapIxRM #-}
-mapIxRM f = mapMaybeIxRM (fmap Just .: f)
-
--- ** With index, outside monad
-
-mapIxL :: (Enum i, Uncons xs x, AsEmpty ys, Cons ys ys y y) => (i -> x -> y) -> xs -> ys
-{-# INLINE mapIxL #-}
-mapIxL f = runIdentity . mapMaybeIxLM (Identity .: Just .: f)
-
-mapIxLR :: (Enum i, Uncons xs x, AsEmpty ys, Snoc ys ys y y) => (i -> x -> y) -> xs -> ys
-{-# INLINE mapIxLR #-}
-mapIxLR f = runIdentity . mapMaybeIxLRM (Identity .: Just .: f)
-
-mapIxR :: (Enum i, Unsnoc xs x, AsEmpty ys, Snoc ys ys y y) => (i -> x -> y) -> xs -> ys
-{-# INLINE mapIxR #-}
-mapIxR f = runIdentity . mapMaybeIxRM (Identity .: Just .: f)
-
--- ** With maybe, inside monad
-mapMaybeLM :: (Uncons xs x, AsEmpty ys, Cons ys ys y y, Monad m) => (x -> m (Maybe y)) -> xs -> m ys
-{-# INLINE mapMaybeLM #-}
-mapMaybeLM f = mapMaybeIxLM @Int (const f)
-
-mapMaybeLRM :: (Uncons xs x, AsEmpty ys, Snoc ys ys y y, Monad m) => (x -> m (Maybe y)) -> xs -> m ys
-{-# INLINE mapMaybeLRM #-}
-mapMaybeLRM f = mapMaybeIxLRM @Int (const f)
-
-mapMaybeRM :: (Unsnoc xs x, AsEmpty ys, Snoc ys ys y y, Monad m) => (x -> m (Maybe y)) -> xs -> m ys
-{-# INLINE mapMaybeRM #-}
-mapMaybeRM f = mapMaybeIxRM @Int (const f)
-
--- | (!) Monad is applied backwards
-mapMaybeRLM :: (Unsnoc xs x, AsEmpty ys, Cons ys ys y y, Monad m) => (x -> m (Maybe y)) -> xs -> m ys
-{-# INLINE mapMaybeRLM #-}
-mapMaybeRLM f = m Empty
+mapsMaybeRLM :: (Unsnoc xs x, Cons ys ys y y, Monad m) => (x -> m (Maybe y)) -> xs -> ys -> m ys
+{-# INLINE mapsMaybeRLM #-}
+mapsMaybeRLM f = m
   where
-    m ys xs = case unsnoc xs of
+    m xs ys = case unsnoc xs of
         Nothing -> return ys
         Just (rs, x) -> do
             y <- f x
-            m (y ?:< ys) rs
+            m rs (y ?:< ys)
 
--- ** With maybe, outside monad
-mapMaybeL :: (Uncons xs x, AsEmpty ys, Cons ys ys y y) => (x -> Maybe y) -> xs -> ys
-{-# INLINE mapMaybeL #-}
-mapMaybeL f = runIdentity . mapMaybeIxLM @Int (const (Identity . f))
-
-mapMaybeLR :: (Uncons xs x, AsEmpty ys, Snoc ys ys y y) => (x -> Maybe y) -> xs -> ys
-{-# INLINE mapMaybeLR #-}
-mapMaybeLR f = runIdentity . mapMaybeIxLRM @Int (const (Identity . f))
-
-mapMaybeR :: (Unsnoc xs x, AsEmpty ys, Snoc ys ys y y) => (x -> Maybe y) -> xs -> ys
-{-# INLINE mapMaybeR #-}
-mapMaybeR f = runIdentity . mapMaybeIxRM @Int (const (Identity . f))
-
-mapMaybeRL :: (Unsnoc xs x, AsEmpty ys, Cons ys ys y y) => (x -> Maybe y) -> xs -> ys
-{-# INLINE mapMaybeRL #-}
-mapMaybeRL f = runIdentity . mapMaybeRLM (Identity . f)
+mkAllMaps
---
+++ Turbo/Internal/TH.hs
@@ -0,0 +1,79 @@
+module Turbo.Internal.TH where
+
+import Language.Haskell.TH
+import Turbo.Operators
+import Turbo.RootPrelude
+
+-- | Makes `map*` functions from `maps*` functions in `MapMaybe`
+mkMap :: [Name] -> Q [Dec]
+mkMap ns = fmap concat $ forM ns \n -> case nameBase n of
+    'm' : 'a' : 'p' : 's' : xs ->
+        let
+            n' = mkName ("map" ++ xs)
+            f = mkName "f"
+            x = mkName "x"
+         in
+            return
+                [ PragmaD (InlineP n' Inline FunLike AllPhases),
+                  FunD
+                    n'
+                    [Clause [VarP f, VarP x] (NormalB (VarE n `AppE` VarE f `AppE` VarE x `AppE` ConE 'Empty)) []]
+                ]
+    _ -> fail $ "Name should start with \"maps\": " ++ nameBase n
+
+{- | Generates map* variants
+    $1: with Index
+    $2: with Maybe
+    $3: with Monad
+    $4: in situ
+    $5: Uncons / Unsnoc
+    $6: Cons / Snoc
+-}
+mkOneMap :: Bool -> Bool -> Bool -> Bool -> Bool -> Bool -> Q [Dec]
+mkOneMap i o m s r b =
+    let
+        regular = (r, b) /= (False, True)
+        lr = (if r then "L" else "R") ++ _if (b /= r) (if b then "L" else "R")
+        n = mkName $ "map" ++ _if s "s" ++ _if o "Maybe" ++ _if i "Ix" ++ lr ++ _if m "M"
+        base = mkName $ "mapsMaybe" ++ (_if regular "Ix") ++ lr ++ "M"
+        f = mkName "f"
+        xs = mkName "xs"
+        ys = mkName "ys"
+        x = mkName "x"
+        j = mkName "i"
+        f' =
+            LamE
+                (_if regular [if i then VarP j else SigP WildP (ConT ''Int)] ++ [VarP x])
+                ( (if m then id else AppE (ConE 'Identity)) $
+                    (if o then id else AppE (ConE 'Just)) $
+                        (if i then (`AppE` (VarE j)) else id) $
+                            (VarE f `AppE` VarE x)
+                )
+     in
+        return
+            [ PragmaD (InlineP n Inline FunLike AllPhases),
+              FunD
+                n
+                [ Clause
+                    (VarP f : VarP xs : _if s [VarP ys])
+                    ( NormalB
+                        ( (if not m then AppE (VarE 'runIdentity) else id)
+                            (VarE base `AppE` f' `AppE` VarE xs `AppE` (if s then VarE ys else ConE 'Empty))
+                        )
+                    )
+                    []
+                ]
+            ]
+  where
+    _if True x = x
+    _if False _ = []
+
+mkAllMaps :: Q [Dec]
+mkAllMaps = mk
+  where
+    tf = [True, False]
+    mk = fmap concat $ forM (tf <&> tf <.&> tf <..&> tf <.:&> tf <..:&> tf) \case
+        (True, True, True, True, _, _) -> return []
+        (True, _, _, _, False, True) -> return []
+        (False, True, True, True, False, True) -> return []
+        (i, o, m, s, r, b) -> mkOneMap i o m s r b
