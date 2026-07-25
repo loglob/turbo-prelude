aea5b833af6f019a0074de43f8a8437a3cf3db1e
loglob <23386237+loglob@users.noreply.github.com>
2026-07-25 02:06:24 +0200

    Cast classes for signed/unsigned casting

--- Turbo/Cast.hs
+++ Turbo/Cast.hs
@@ -2,97 +2,40 @@
 
 module Turbo.Cast where
 
-import Data.Bits
-import GHC.Err (error)
 import GHC.Exts
 import Turbo.RootPrelude
 import Turbo.CastTH
+import Data.Kind (Type)
 
--- * Sign Elimination
-
-{- | Bit-casts int to word
-
-(!) Partial if the argument is negative
--}
-unsign :: Int -> Word
-unsign (I# i) = if isTrue# (i <# 0#) then error "Input to unsign is negative" else W# (int2Word# i)
-
-{- | Bit-casts 8-bit signed to unsigned
-
-(!) Partial if the argument is negative
--}
-unsign8 :: Int8 -> Word8
-unsign8 (I8# i) = if isTrue# (i `ltInt8#` 0#Int8) then error "Input to unsign8 is negative" else W8# (int8ToWord8# i)
-
-{- | Bit-casts 16-bit signed to unsigned
-
-(!) Partial if the argument is negative
--}
-unsign16 :: Int16 -> Word16
-unsign16 (I16# i) = if isTrue# (i `ltInt16#` 0#Int16) then error "Input to unsign16 is negative" else W16# (int16ToWord16# i)
-
-{- | Bit-casts 32-bit signed to unsigned
-
-(!) Partial if the argument is negative
--}
-unsign32 :: Int32 -> Word32
-unsign32 (I32# i) = if isTrue# (i `ltInt32#` 0#Int32) then error "Input to unsign32 is negative" else W32# (int32ToWord32# i)
-
-{- | Bit-casts 64-bit signed to unsigned
-
-(!) Partial if the argument is negative
--}
-unsign64 :: Int64 -> Word64
-unsign64 (I64# i) = if isTrue# (i `ltInt64#` 0#Int64) then error "Input to unsign64 is negative" else W64# (int64ToWord64# i)
-
--- * Sign Introduction
-
-{- | Bit-casts a word to an int.
-
-(!) Partial if the value does not fit into the bounds of Int
--}
-sign :: Word -> Int
-sign (W# w) =
-    let !(I# b) = finiteBitSize (0 :: Int)
-     in if isTrue# (word2Int# (w `uncheckedShiftRL#` (b -# 1#)))
-            then error "Input to sign exceeds the bounds of Int"
-            else I# (word2Int# w)
-
-{- | Bit-casts an 8-bit unsigned to signed
-
-(!) Partial if the value does not fit into 7 bits
--}
-sign8 :: Word8 -> Int8
-sign8 (W8# w) = if isTrue# (w `gtWord8#` 0x7F#Word8) then error "Input to sign8 exceeds the bounds of Int8" else I8# (word8ToInt8# w)
-
-{- | Bit-casts a 16-bit unsigned to signed
-
-(!) Partial if the value does not fit into 15 bits
--}
-sign16 :: Word16 -> Int16
-sign16 (W16# w) = if isTrue# (w `gtWord16#` 0x7FFF#Word16) then error "Input to sign16 exceeds the bounds of Int16" else I16# (word16ToInt16# w)
-
-{- | Bit-casts a 32-bit unsigned to signed
-
-(!) Partial if the value does not fit into 31 bits
--}
-sign32 :: Word32 -> Int32
-sign32 (W32# w) = if isTrue# (w `gtWord32#` 0x7FFF#Word32) then error "Input to sign32 exceeds the bounds of Int32" else I32# (word32ToInt32# w)
-
-{- | Bit-casts a 64-bit unsigned to signed
-
-(!) Partial if the value does not fit into 31 bits
--}
-sign64 :: Word64 -> Int64
-sign64 (W64# w) = if isTrue# (w `gtWord64#` 0x7FFF#Word64) then error "Input to sign64 exceeds the bounds of Int64" else I64# (word64ToInt64# w)
+class Boxed (unboxed :: TYPE kind) (boxed :: Type) | boxed -> unboxed, unboxed -> boxed where
+    box :: unboxed -> boxed
+    unbox :: boxed -> unboxed
 
 -- | The property that `long` is a bitfield with more bits than `short`.
+--   Implements safe widening and narrowing
 class (long :: TYPE a) :>: (short :: TYPE b) where
-    -- | Bit-exact widening of a bit-field. 
+    -- | Extends a bit field to become a wider type.
     --   Performs sign extension only when widening from a signed type to another signed type.
+    --   Widening a signed type to an unsigned type is not permitted.
     extend :: short -> long
     -- | Truncates a bit-field into a smaller type, discarding the most significant bits.
-    --   May produce negative numbers for signed result types.
+    --   Total but may produce negative numbers for signed result types.
     narrow :: long -> short
 
-makeInstances
+$makeExtendInstances
+
+-- | Expresses that two bit-fields are the same width, but one has a sign bit and the other doesn't
+class IsSigned (signed :: TYPE a) (unsigned :: TYPE b) | signed -> unsigned, unsigned -> signed where
+    -- | Converts a signed number to an equally wide unsigned type
+    --   Undefined for values that exceed the target type's capacity
+    sign :: unsigned -> signed
+    -- | Like sign_ but saturates (becomes the maximum value) when the input exceeds the bounds
+    signSat :: unsigned -> signed
+
+    -- | Converts a signed number to an equally wide unsigned type
+    --   Undefined for negative values
+    unsign :: signed -> unsigned
+    -- | Like sign_ but saturates (becomes 0) when the input is negative
+    unsignSat :: signed -> unsigned
+
+$makeIsSignedInstances
--- Turbo/CastTH.hs
+++ Turbo/CastTH.hs
@@ -1,9 +1,22 @@
-module Turbo.CastTH (makeInstances) where
+module Turbo.CastTH (makeExtendInstances, makeIsSignedInstances) where
 import Language.Haskell.TH
 import GHC.Err (undefined, error)
 import Turbo.RootPrelude
+import Turbo.Extra (both)
+import GHC.Exts ((<#))
+import Data.Bits (Bits(shiftL, shiftR))
 
 data Width = W8 | W16 | W32 | W | W64 deriving (Eq, Show, Enum, Bounded, Ord)
+
+bits :: Width -> Int
+bits W8  = 8
+bits W16 = 16
+bits W32 = 32
+bits W   = undefined
+bits W64 = 64
+
+widths :: [Width]
+widths = [minBound .. maxBound] :: [Width]
 
 suffix :: Width -> String
 suffix x = case show x of
@@ -36,11 +49,18 @@ directCast (T fW _ fS) (T tW _ tS) = VarE $ mkName $ f where
 boxCtor :: T -> Name
 boxCtor (T w _ s) = mkName $ (if s then "I" else "W") ++ suffix w ++ "#"
 
+boxPat :: T -> Name -> Pat
+boxPat t name = if boxed t then BangP (ConP (boxCtor t) [] [VarP name]) else VarP name
+
 convert :: T -> T -> Exp -> Exp
 convert from to
- | from == to = undefined
+ | from == to = id
 -- unbox everything
- | boxed from = error "Input should be unboxed earlier!"
+ | boxed from = \x -> let
+        tmp = mkName "y"
+        from' = from{ boxed = False }
+    in 
+        LetE [ ValD (boxPat from tmp) (NormalB x) [] ] (convert from' to $ VarE tmp)
  | boxed to   = AppE (ConE (boxCtor to)) . convert from to{ boxed = False }
 -- fix signedness
  | signed from /= signed to = case()of 
@@ -55,37 +75,86 @@ convert from to
 -- primitiveCast from to = (VarE $ mkName $ "int" ++ from ++ "ToInt" ++ to ++ "#")
 -- primitiveCast from to = (primitiveCast from "") `comp` (primitiveCast "" to)
 
-mkInst :: T -> T -> Dec
-mkInst long short = i
+func :: String -> (Name -> Pat) -> (Exp -> Exp) -> Dec
+func name pat body = FunD (mkName name) [
+    Clause [pat tmp] (NormalB $ body $ VarE tmp) []
+ ] where
+    tmp = mkName "x"
+
+mkExtend :: T -> T -> Dec
+mkExtend long short = i
  where
     i = InstanceD Nothing [] (cls `AppT` t long `AppT` t short) [
-        method "extend" $ unbox short $ convert short{ boxed=False } long ,
-        method "narrow" $ unbox long  $ convert long{ boxed=False } short
+        func "extend" (boxPat short) (convert short{ boxed=False } long),
+        func "narrow" (boxPat long)  (convert long{ boxed=False }  short)
      ]
-
-    method :: String -> Clause -> Dec
-    method n c = FunD (mkName n) [ c ]
-
-    unbox :: T -> (Exp -> Exp) -> Clause
-    unbox i b = Clause [pat i] (NormalB $ b $ VarE arg) []
-
-    arg = mkName "x"
-    -- boxing-agnostic argument pattern
-    pat :: T -> Pat
-    pat t = if boxed t then ConP (boxCtor t) [] [VarP arg] else VarP arg
     cls = ConT $ mkName ":>:"
 
 followPairs :: [a] -> [(a,a)]
 followPairs xs = [ (x,y) | (x:rest) <- tails xs, y <- rest ]
 
-makeInstances :: Q [Dec]
-makeInstances = return do
-    (short,long) <- followPairs [minBound .. maxBound]
+makeExtendInstances :: Q [Dec]
+makeExtendInstances = return do
+    (short,long) <- followPairs widths
     sb <- [ True, False ]
     lb <- [ True, False ]
     -- widening a short unsigned into a long signed is valid
     (si,li) <- [ (True, True), (False, False), (False,True) ]
     let s = T short sb si
     let l = T long lb li
+    return (mkExtend l s)
 
-    return (mkInst l s)
+constant :: T -> Integer -> Exp
+constant t x
+ | boxed t   = LitE $ IntegerL x -- these are instances of `Number`
+ | otherwise = convert t{width = W} t $ LitE $ (if signed t then IntPrimL else WordPrimL) x -- TH cannot generate e.g. `0#Int32` ?
+
+-- | Generates an expression that evaluates to True if $2 (of type $1) is negative and False otherwise
+isNegative :: T -> Exp -> Exp
+isNegative t n
+ | boxed t   = InfixE (Just n) (VarE '(<)) (Just zero)
+-- isTrue# (i `ltInt16#` 0#Int16)
+ | otherwise = (VarE 'isTrue#) `AppE` (VarE lt `AppE` n `AppE` zero)
+ where
+    lt | width t == W = '(<#)
+       | otherwise    = mkName$ "lt" ++ show t
+    zero = constant t 0
+
+-- | Generates an expression that evaluates to the maximum value of a type
+maxSafeValue :: T -> Exp
+maxSafeValue t 
+ | boxed t      = VarE 'maxBound -- are always bounded
+ | width t == W = let t' = t{ boxed=True } in convert t' t $ maxSafeValue t' -- forward lifted definition
+ -- these have fixed bit size
+ | otherwise    = let
+    b = bits (width t)
+    tooLong = (1 :: Integer) `shiftL` b
+    fullSet = tooLong - 1
+    woSign = fullSet `shiftR` 1
+ in 
+    constant t woSign
+
+makeIsSignedInstances :: Q [Dec]
+makeIsSignedInstances = return do
+    w <- widths
+    boxed <- [ True, False ]
+    let (signed, unsigned) = both (T w boxed) (True, False)
+    -- | name has to be late-resolved
+    let sig = ConT (mkName "IsSigned") `AppT` t signed `AppT` t unsigned
+    -- | Makes an unsign* function, that inserts $2 when the input was negative
+    let mkUnsign x fallback = CondE (isNegative signed x) fallback (convert signed unsigned x)
+    let tmp = mkName "y"
+    -- | Makes a sign* function, that inserts $2 when the conversion overflows
+    --   Testing sign after conversion is sufficient since the bit-width doesn't change
+    let mkSign x fallback = LetE [
+            ValD (VarP tmp) (NormalB$ convert unsigned signed x) []
+         ]$ CondE (isNegative signed$ VarE tmp) fallback (VarE tmp)
+
+    let mkError msg = (VarE 'error) `AppE` LitE (StringL msg)
+    
+    return$ InstanceD Nothing [] sig [
+        func "sign" VarP \x -> mkSign x (mkError "argument of sign is negative"),
+        func "signSat" VarP \x -> mkSign x (maxSafeValue signed),
+        func "unsign" VarP \x -> mkUnsign x (mkError "argument of unsign is out of bounds"),
+        func "unsignSat" VarP \x -> mkUnsign x (constant unsigned 0)
+     ]
