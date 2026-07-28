c7b4b02af06aa406ff94cefe7059e4d9c477ca3d
loglob <23386237+loglob@users.noreply.github.com>
2026-07-28 21:57:06 +0200

    Boxed instances for remaining GHC primitives

--- Turbo/Cast.hs
+++ Turbo/Cast.hs
@@ -7,11 +7,27 @@ import Turbo.RootPrelude
 import Turbo.Cast.Classes as E
 import Turbo.Cast.TH
 import Data.Primitive
+import GHC.StableName (StableName)
+import GHC.Conc (TVar, ThreadId)
+import GHC.Weak (Weak)
+import GHC.Stack.CloneStack (StackSnapshot)
+import GHC.Stable (StablePtr)
 
 $(deriveBoxed ''SmallArray)
+$(deriveBoxed ''SmallMutableArray)
 $(deriveBoxed ''Array)
-$(deriveBoxed ''ByteArray)
 $(deriveBoxed ''MutableArray)
+$(deriveBoxed ''ByteArray)
+$(deriveBoxed ''MutableByteArray)
+
+$(deriveBoxed ''MutVar)
+$(deriveBoxed ''StableName)
+$(deriveBoxed ''StablePtr)
+$(deriveBoxed ''StackSnapshot)
+$(deriveBoxed ''ThreadId)
+$(deriveBoxed ''TVar)
+$(deriveBoxed ''Weak)
+
 $(deriveBoxed ''Char)
 $(deriveBoxed ''Float)
 $(deriveBoxed ''Double)
@@ -19,3 +35,7 @@ $(deriveBoxed ''Double)
 $makeBoxedIntegers
 $makeExtend
 $makeIsSigned
+
+instance Boxed Addr# (Ptr ()) where
+    box = Ptr
+    unbox (Ptr x) = x
